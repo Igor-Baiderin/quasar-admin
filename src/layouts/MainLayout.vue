10925e7871f153a6be2706fdd5118fbc729fc9bc
Игорь Байдерин <ibaid@mail.ru>
2023-10-13 11:22:14 +0500

    init

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,390 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          @click="toggleLeftDrawer"
+          icon="menu"
+          aria-label="Menu"
+        />
+        <q-toolbar-title>
+          Админка
+        </q-toolbar-title>
+        <q-space/>
+        <div class="q-gutter-sm row items-center no-wrap">
+                    <q-btn round dense flat color="white" :icon="$q.fullscreen.isActive ? 'fullscreen_exit' : 'fullscreen'"
+                           @click="$q.fullscreen.toggle()"
+                           v-if="$q.screen.gt.sm">
+                    </q-btn>
+          <q-btn round dense flat color="white" icon="fab fa-github" type="a"
+                 href="https://github.com/pratik227/quasar-admin" target="_blank">
+          </q-btn>
+          <q-btn round dense flat style="color:red !important;" type="a" href="https://github.com/sponsors/pratik227"
+                 target="_blank">
+            <i class="fa fa-heart fa-2x fa-beat"></i>
+          </q-btn>
+          <q-btn round dense flat color="white" icon="notifications">
+            <q-badge color="red" text-color="white" floating>
+              5
+            </q-badge>
+            <q-menu
+            >
+              <q-list style="min-width: 100px">
+                <messages></messages>
+                <q-card class="text-center no-shadow no-border">
+                  <q-btn label="View All" style="max-width: 120px !important;" flat dense
+                         class="text-indigo-8"></q-btn>
+                </q-card>
+              </q-list>
+            </q-menu>
+          </q-btn>
+          <q-btn round flat>
+            <q-avatar size="26px">
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+            </q-avatar>
+          </q-btn>
+        </div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      class="bg-primary text-white"
+    >
+      <q-list>
+        <q-item to="/" active-class="q-item-no-link-highlighting">
+          <q-item-section avatar>
+            <q-icon name="dashboard"/>
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Домашняя админки</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item to="/Dashboard2" active-class="q-item-no-link-highlighting">
+          <q-item-section avatar>
+            <q-icon name="dashboard"/>
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>CRM Админка</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-expansion-item
+          icon="pages"
+          label="Страницы"
+        >
+          <q-list class="q-pl-lg">
+            <q-item to="/Login-1" active-class="q-item-no-link-highlighting">
+              <q-item-section avatar>
+                <q-icon name="email"/>
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>Регистрация</q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-item to="/Lock" active-class="q-item-no-link-highlighting">
+              <q-item-section avatar>
+                <q-icon name="lock"/>
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>Экран блокировки</q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-item to="/Lock-2" active-class="q-item-no-link-highlighting">
+              <q-item-section avatar>
+                <q-icon name="lock"/>
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>Экран блокировки - 2</q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-item to="/Pricing" active-class="q-item-no-link-highlighting">
+              <q-item-section avatar>
+                <q-icon name="list"/>
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>Цены</q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-item-label header class="text-weight-bolder text-white">Generic</q-item-label>
+            <q-item to="/Profile" active-class="q-item-no-link-highlighting">
+              <q-item-section avatar>
+                <q-icon name="person"/>
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>Профиль пользователя</q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-item to="/Maintenance" active-class="q-item-no-link-highlighting">
+              <q-item-section avatar>
+                <q-icon name="settings"/>
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>Обслуживание</q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-expansion-item>
+        <q-expansion-item
+          icon="map"
+          label="Карта"
+        >
+          <q-list class="q-pl-lg">
+            <q-item to="/Map" active-class="q-item-no-link-highlighting">
+              <q-item-section avatar>
+                <q-icon name="map"/>
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>Карта</q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-item to="/MapMarker" active-class="q-item-no-link-highlighting">
+              <q-item-section avatar>
+                <q-icon name="location_on"/>
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>Маркер карты</q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-item to="/StreetView" active-class="q-item-no-link-highlighting">
+              <q-item-section avatar>
+                <q-icon name="streetview"/>
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>Просмотр улиц</q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-expansion-item>
+
+        <q-item to="/Mail" active-class="q-item-no-link-highlighting">
+          <q-item-section avatar>
+            <q-icon name="email"/>
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Почта</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item to="/directory" active-class="q-item-no-link-highlighting">
+          <q-item-section avatar>
+            <q-icon name="card_giftcard"/>
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Каталог</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item to="/TreeTable" active-class="q-item-no-link-highlighting">
+          <q-item-section avatar>
+            <q-icon name="list"/>
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Иерархия</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item to="/Charts" active-class="q-item-no-link-highlighting">
+          <q-item-section avatar>
+            <q-icon name="insert_chart"/>
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Графики</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item to="/Footer" active-class="q-item-no-link-highlighting">
+          <q-item-section avatar>
+            <q-icon name="info"/>
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Нижний колонтитул</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item to="/CardHeader" active-class="q-item-no-link-highlighting">
+          <q-item-section avatar>
+            <q-icon name="card_giftcard"/>
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Заголовок карточки</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item to="/Cards" active-class="q-item-no-link-highlighting">
+          <q-item-section avatar>
+            <q-icon name="card_giftcard"/>
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Карточки</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item to="/Tables" active-class="q-item-no-link-highlighting">
+          <q-item-section avatar>
+            <q-icon name="table_chart"/>
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Таблицы</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item to="/Contact" active-class="q-item-no-link-highlighting">
+          <q-item-section avatar>
+            <q-icon name="person"/>
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Контакты</q-item-label>
+          </q-item-section>
+        </q-item>
+        <!--        not completed-->
+        <q-item to="/Calendar" active-class="q-item-no-link-highlighting">
+          <q-item-section avatar>
+            <q-icon name="date_range"/>
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Календарь</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <!--        not completed-->
+        <!--        <q-item to="/Taskboard" active-class="q-item-no-link-highlighting">-->
+        <!--          <q-item-section avatar>-->
+        <!--            <q-icon name="done"/>-->
+        <!--          </q-item-section>-->
+        <!--          <q-item-section>-->
+        <!--            <q-item-label>Taskboard</q-item-label>-->
+        <!--          </q-item-section>-->
+        <!--        </q-item>-->
+
+        <q-item to="/Pagination" active-class="q-item-no-link-highlighting">
+          <q-item-section avatar>
+            <q-icon name="date_range"/>
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Пагинация</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item to="/Ecommerce" active-class="q-item-no-link-highlighting">
+          <q-item-section avatar>
+            <q-icon name="shopping_cart"/>
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Каталоги продукции</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-expansion-item
+          icon="menu_open"
+          label="Вложенность меню"
+        >
+          <q-item class="q-ml-xl" active-class="q-item-no-link-highlighting">
+            <q-item-section>
+              <q-item-label>Level 1</q-item-label>
+            </q-item-section>
+          </q-item>
+          <q-expansion-item
+            :header-inset-level="0.85"
+            label="Level 2"
+          >
+            <q-item class="q-ml-xl" style="margin-left: 55px  !important;" active-class="q-item-no-link-highlighting">
+              <q-item-section>
+                <q-item-label>Level 2.1</q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-expansion-item
+              :header-inset-level="1"
+              label="Level 2.2"
+            >
+              <q-item style="margin-left: 65px  !important;" active-class="q-item-no-link-highlighting">
+                <q-item-section>
+                  <q-item-label>Level 2.2.1</q-item-label>
+                </q-item-section>
+              </q-item>
+              <q-item style="margin-left: 65px  !important;" active-class="q-item-no-link-highlighting">
+                <q-item-section>
+                  <q-item-label>Level 2.2.2</q-item-label>
+                </q-item-section>
+              </q-item>
+            </q-expansion-item>
+          </q-expansion-item>
+        </q-expansion-item>
+        <q-item to="/Checkout" active-class="q-item-no-link-highlighting">
+          <q-item-section avatar>
+            <q-icon name="check_circle_outline"/>
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Выход</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container class="bg-grey-2">
+      <router-view/>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink.vue'
+import Messages from "./Messages.vue";
+
+import {defineComponent, ref} from 'vue'
+import {useQuasar} from "quasar";
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink,
+    Messages
+  },
+
+  setup() {
+    const leftDrawerOpen = ref(false)
+    const $q = useQuasar()
+
+    return {
+      $q,
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
+
+<style>
+
+/* FONT AWESOME GENERIC BEAT */
+.fa-beat {
+  animation: fa-beat 5s ease infinite;
+}
+
+@keyframes fa-beat {
+  0% {
+    transform: scale(1);
+  }
+  5% {
+    transform: scale(1.25);
+  }
+  20% {
+    transform: scale(1);
+  }
+  30% {
+    transform: scale(1);
+  }
+  35% {
+    transform: scale(1.25);
+  }
+  50% {
+    transform: scale(1);
+  }
+  55% {
+    transform: scale(1.25);
+  }
+  70% {
+    transform: scale(1);
+  }
+}
+
+</style>
